5dcd1379671ffdec5a1b16dc83646837eb983e62
Neix20 <txen2000@gmail.com>
2026-01-24 16:14:14 +0800

    feat: Update Plugin

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,7 +1,16 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
-  {
-    "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-plenary" } },
+  "nvim-neotest/neotest",
+  optional = true,
+  dependencies = {
+    "nvim-neotest/neotest-python",
+  },
+  opts = {
+    adapters = {
+      ["neotest-python"] = {
+        -- Here you can specify the settings for the adapter, i.e.
+        -- runner = "pytest",
+        -- python = ".venv/bin/python",
+      },
+    },
   },
 }
