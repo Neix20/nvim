973af6ee682af223eacae5f9aac7fc95b473b269
Neix20 <txen2000@gmail.com>
2026-03-24 00:50:40 +0800

    feat: Added Image Preview to oil.nvim. Using oil.nvim as our default File Manager

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,24 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function()
+    local oil = require("oil")
+    oil.setup({
+      default_file_explorer = true,
+      columns = {
+        "icon",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    local function open_oil_cwd()
+      if oil.get_current_dir() then
+        oil.close()
+      else
+        oil.open(vim.fn.getcwd())
+      end
+    end
+    vim.keymap.set("n", "<leader>e", open_oil_cwd, { desc = "Open oil" })
+  end,
 }
